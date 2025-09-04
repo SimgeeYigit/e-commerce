4778ffad79883fb32376a80c5e91e4a7e0e1012d
Simge Yiğit <simgeyigit98@gmail.com>
2025-09-04 23:02:23 +0300

    T02: Home Page #2
    Refactor Featured component layout and improve responsiveness

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,21 +3,31 @@ import two from "../assets/homepage/featured/2.svg";
 
 function Featured() {
     return (
-        <div className="flex items-center justify-center mx-[9.876rem] py-20">
-            <div className="flex gap-4 w-1/2">
-                <img src={two} alt="Featured Product 1" />
-                <img src={one} alt="Featured Product 2" />
+        <div className="flex flex-col-reverse items-center justify-center mx-0 md:mx-[9.876rem] py-20 md:flex-row gap-[3.125rem]">
+            <div className="flex gap-4 md:w-1/2 md:justify-start justify-center">
+                <img src={two} alt="Featured Product 1" className="w-1/3 h-auto object-cover md:w-auto" />
+                <img src={one} alt="Featured Product 2" className="w-1/2 h-auto object-cover md:w-auto" />
             </div>
-            <div className="flex flex-col gap-4 pl-10">
+            <div className="flex flex-col gap-4 md:pl-10 md:pr-0">
                 <h5 className="font-montserrat font-bold text-[#23A6F0] text-base">Featured Products</h5>
-                <h2 className="font-montserrat font-bold text-[#252B42] text-[2.5rem]">We love what we do</h2>
-                <p className="font-montserrat font-normal text-[#737373] text-sm">Problems trying to resolve the conflict between<br />
+                <h2 className="font-montserrat font-bold text-[#252B42] text-[2.5rem]">We love <br className="block md:hidden" />what we do</h2>
+                <p className="font-montserrat font-normal text-[#737373] text-sm hidden md:block">Problems trying to resolve the <br className="block md:hidden" />conflict between<br />
                     the two major realms of Classical physics:<br />
                     Newtonian mechanics.
-                    <br /> <br/>
+                    <br /> <br />
                     Problems trying to resolve the conflict between<br />
                     the two major realms of Classical physics:<br />
                     Newtonian mechanics </p>
+
+                <p className="font-montserrat font-normal text-[#737373] text-sm block md:hidden">Problems trying to resolve the <br />
+                        conflict between the two major<br />
+                        realms of Classical physics:<br />
+                        Newtonian mechanics.
+                        <br /> <br />
+                        Problems trying to resolve the <br />
+                        conflict between the two major<br />
+                        realms of Classical physics:<br />
+                        Newtonian mechanics. </p>
             </div>
         </div>
     );
