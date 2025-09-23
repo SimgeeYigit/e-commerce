c4894f1b95c2790392ca558e21d2eb4edd096c6a
Simge Yiğit <simgeyigit98@gmail.com>
2025-09-24 00:18:00 +0300

    ***

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,11 +1,11 @@
 import Brands from "../layout/Brands";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
-import Cloths from "../components/shoppage/Cloths";
-import Show from "../components/shoppage/Show";
-import Products from "../components/shoppage/Products";
+import Cloths from "../components/shoppage/Cloths.jsx";
+import Show from "../components/shoppage/Show.jsx";
+import Products from "../components/shoppage/Products.jsx";
 import { useState } from "react";
-import Pagination from "../components/shoppage/Pagination";
+import Pagination from "../components/shoppage/Pagination.jsx";
 
 function ShopPage() {
     const [showColumns, setShowColumns] = useState(false);
